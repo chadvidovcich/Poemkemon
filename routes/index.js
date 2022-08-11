8bed1f9f6312a44803ece515fcf0a6bc41e7e95c
chadvidovcich <chad.vidovcich@gmail.com>
2022-08-11 08:43:39 -0700

    fixes #17

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest} = require('../middleware/auth')
 
+// bring in Issue model
+const Issue = require('../models/Issue')
+
 // @desc    Login/Landing page
 // @route   GET /
 router.get('/', ensureGuest, (req,res) => {
@@ -12,11 +15,18 @@ router.get('/', ensureGuest, (req,res) => {
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get('/dashboard', ensureAuth, (req,res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-        image: req.user.image,
-    })
+router.get('/dashboard', ensureAuth, async (req,res) => {
+    try {
+        const issues = await Issue.find({ user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            issues
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+    
 })
 
 module.exports = router
