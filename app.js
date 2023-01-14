8bb5ba695056af83bc6c67b998b5dfa7581fd0b4
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-01-13 17:19:33 -0800

    create clickable link in console when running

--- app.js
+++ app.js
@@ -1,86 +1,106 @@
-const path = require('path')
-const express = require('express')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const connectDB = require('./config/db')
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const connectDB = require('./config/db');
 
 // Load config
-dotenv.config({ path: './config/config.env'})
+dotenv.config({ path: './config/config.env' });
 
 // Passport config
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 // Connect mongoDB database
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 // Body Parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Method override
-app.use(methodOverride(function (req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = req.body._method
-    delete req.body._method
-    return method
-  }
-}))
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 // Logging
-if(process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Handlebars Helpers
-const {formatDate, truncate, stripTags, editIcon, select} = require('./helpers/hbs')
-
-// Handlebars
-app.engine('.hbs', exphbs.engine({ helpers: {
+const {
   formatDate,
   truncate,
   stripTags,
   editIcon,
   select,
-}, defaultLayout: 'main', extname: '.hbs' }));
+} = require('./helpers/hbs');
+
+// Handlebars
+app.engine(
+  '.hbs',
+  exphbs.engine({
+    helpers: {
+      formatDate,
+      truncate,
+      stripTags,
+      editIcon,
+      select,
+    },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
 app.set('view engine', '.hbs');
 
 // Sessions
-app.use(session({
+app.use(
+  session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URI
-    })
-  }))
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
 
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set global variable for user
 app.use(function (req, res, next) {
-  res.locals.user = req.user || null
-  next()
-})
-
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/', require('./routes/index'))
-app.use('/auth', require('./routes/auth'))
-app.use('/issues', require('./routes/issues'))
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/issues', require('./routes/issues'));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode.`),
+  console.log(`Visit http://localhost:${PORT}/`)
+);
